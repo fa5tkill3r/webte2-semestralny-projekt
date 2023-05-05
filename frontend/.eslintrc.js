5813af2a2d7146391db71fceaa515c4e9f8d56b1
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-05 22:28:47 +0200

    Add frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: ['vue', 'prettier'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'eslint-config-prettier',
+    'eslint:recommended',
+  ],
+}
