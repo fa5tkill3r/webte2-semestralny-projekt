ca55250ec9de6f43539d8dbe509e2b4de2b50a8e
Radovan Križanovský <xkrizanovsky@stuba.sk>
2023-05-15 19:54:51 +0200

    Language button, flag toggle

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -3,45 +3,61 @@
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
 
-      {{$t('finalAssignment')}}
+      {{ $t('finalAssignment') }}
     </v-app-bar-title>
 
     <v-spacer />
     <div v-if="!store.user">
-      <v-btn type="submit" @click="$i18n.locale = 'en'">{{
-        $t('english')
-      }}</v-btn>
-      &nbsp;
-      <v-btn type="submit" @click="$i18n.locale = 'sk'">{{
-        $t('slovak')
-      }}</v-btn>
-      <v-btn to="/login"> {{$t('login')}} </v-btn>
-      <v-btn to="/register"> {{$t('register')}} </v-btn>
+      <v-btn @click="toggleImage">
+        <v-img
+          style="border-style: solid; border-color: black; border-width: 1px"
+          width="25"
+          :src="currentImage"
+          alt="Image"
+        />
+      </v-btn>
+      <v-btn to="/login"> {{ $t('login') }} </v-btn>
+      <v-btn to="/register"> {{ $t('register') }} </v-btn>
     </div>
     <div v-else>
-      <v-btn type="submit" @click="$i18n.locale = 'en'">{{
-        $t('english')
-      }}</v-btn>
-      &nbsp;
-      <v-btn type="submit" @click="$i18n.locale = 'sk'">{{
-        $t('slovak')
-      }}</v-btn>
-      <v-btn to="/assignments"> {{$t('Assignments')}} </v-btn>
-      <v-btn to="/generate"> {{$t('Generate')}} </v-btn>
-
-      <v-btn :loading="loading" @click="logout"> {{$t('Logout')}} </v-btn>
+      <v-btn @click="toggleImage">
+        <v-img
+          style="border-style: solid; border-color: black; border-width: 1px"
+          width="25"
+          :src="currentImage"
+          alt="Image"
+        />
+      </v-btn>
+      <v-btn to="/assignments"> {{ $t('Assignments') }} </v-btn>
+      <v-btn to="/generate"> {{ $t('Generate') }} </v-btn>
+      <v-btn :loading="loading" @click="logout"> {{ $t('Logout') }} </v-btn>
     </div>
   </v-app-bar>
 </template>
 
 <script setup>
 import { useAppStore } from '@/store/app'
-import { ref } from 'vue'
+import { ref, getCurrentInstance } from 'vue'
 import router from '@/router'
+import enImage from '@/assets/en.jpg'
+import skImage from '@/assets/sk.jpg'
+
+const { proxy } = getCurrentInstance()
 
 const store = useAppStore()
 
 const loading = ref(false)
+
+const currentImage = ref(enImage)
+
+const toggleImage = () => {
+  proxy.$i18n.locale = proxy.$i18n.locale === 'en' ? 'sk' : 'en'
+  if (proxy.$i18n.locale === 'sk') {
+    currentImage.value = enImage
+  } else {
+    currentImage.value = skImage
+  }
+}
 
 const logout = async () => {
   try {
