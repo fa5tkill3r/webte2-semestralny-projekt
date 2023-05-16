ed979b75a2015f85498c8ed2ecd90d937671940a
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-16 20:16:26 +0200

    Change appIcon

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+      <v-icon icon="mdi-bookshelf" />
 
       {{ $t('finalAssignment') }}
     </v-app-bar-title>
