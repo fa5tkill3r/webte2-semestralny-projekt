2d3bc5a6b6b7ce44fe8a946cda6f5b6546bf07d2
andreasigeti <xsigetiova@stuba.sk>
2023-05-17 00:24:38 +0200

    changed main

--- backend/app/Python/main.py
+++ backend/app/Python/main.py
@@ -1,41 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import json
 
 import sympy
+from sympy.parsing.latex import parse_latex
+
+import json
+
+
 
 class Item(BaseModel):
     expr1: str
     expr2: str 
 
-app = FastAPI()
-
-@app.get("/hello")
-def read_root():
-    return {"result": "Hello world"}
+app = FastAPI(ssl_keyfile="/home/xsigetiova/webte.fei.stuba.sk.key", ssl_certfile="/home/xsigetiova/webte_fei_stuba_sk.pem")
 
 
-@app.get("/porovnaj")
-async def read_porovnaj():
-    s = sympy.Symbol('s')
-    expr1 = 2/(s+4)
-    expr2 = 4/(2*s+6)
-    simplified_expr1 = sympy.simplify(expr1)
-    simplified_expr2 = sympy.simplify(expr2)
-    if simplified_expr1 == simplified_expr2:
-        result = 1
-    else:
-        result = 0
-    return {"result":result}  
 
-
-@app.post("/porovnaj2")
+@app.post("/compare")
 async def create_porovnaj2(expr:Item):
-    #item_dict = item.dict()
-    #async def read_porovnaj2(expr1:str="2/(s+4)",expr2:str="4/(2*s+8)"):
-    # s = sympy.Symbol('s')
-    # expr1 = eval(expr1)
-    # expr2 = eval(expr2)
+    expr.expr1 = parse_latex(expr.expr1)
+    expr.expr2 = parse_latex(expr.expr2)
     simplified_expr1 = sympy.simplify(expr.expr1)
     simplified_expr2 = sympy.simplify(expr.expr2)
     if simplified_expr1 == simplified_expr2:
