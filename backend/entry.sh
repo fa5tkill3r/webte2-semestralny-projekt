c9a34efb53d2130e262a2ed787bb52c428035dc4
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-17 00:18:54 +0200

    Fix backend not waiting for mysql

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+until mysql -h"mysql" -u"webte" -p"webte" -e "quit" >/dev/null 2>&1; do
+    echo "Waiting for MySQL to be available..."
+    sleep 1
+done
+
 if [ ! -f /var/www/html/public/migration.temp ]; then
-    php artisan migrate
+    php artisan migrate --force
     touch /var/www/html/public/migration.temp
 fi
+
+tail -f /dev/null
