19e5cb0d8cf4b82e15d00a0aa5e7b1d6cae607de
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-17 03:13:29 +0200

    Docker file almost done

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-until mysql -h"mysql" -u"webte" -p"webte" -e "quit" >/dev/null 2>&1; do
-    echo "Waiting for MySQL to be available..."
-    sleep 1
-done
+#until mysql -h"mysql" -u"webte" -p"webte" -e "quit" >/dev/null 2>&1; do
+#    echo "Waiting for MySQL to be available..."
+#    sleep 1
+#done
 
 if [ ! -f /var/www/html/public/migration.temp ]; then
     php artisan migrate --force
