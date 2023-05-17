e0bfff73c0c30d5755dbeeab6aa8c72758a9f6bd
Radovan Križanovský <xkrizanovsky@stuba.sk>
2023-05-17 03:33:29 +0200

    Dashboard data and design

--- frontend/src/views/Teacher/TeacherView.vue
+++ frontend/src/views/Teacher/TeacherView.vue
@@ -1,93 +1,144 @@
 <template>
   <v-container>
-    <v-data-table
-      :headers="headers"
-      :items="students"
-    ></v-data-table>
-  </v-container>
+    <v-card class="dashboard-card">
+      <v-card-title>Students</v-card-title>
+      <v-card-text>
+        <v-data-table :headers="headers" :items="students"></v-data-table>
+      </v-card-text>
+    </v-card>
 
-  <v-container>
-    <v-data-table :headers="headersTwo" :items="tasks" item-key="id">
-      <template #header="{ props }">
-        <tr>
-          <th>
-            <v-checkbox v-model="props.all" :indeterminate="props.indeterminate"></v-checkbox>
-          </th>
-          <th>{{ headersTwo[1].title }}</th>
-        </tr>
-      </template>
-      <template #item="{ item }">
-        <tr>
-          <td>
-            <v-checkbox :value="isSelected(item)" @input="toggleSelection(item)"></v-checkbox>
-          </td>
-          <td>{{ item.name }}</td>
-        </tr>
-      </template>
-    </v-data-table>
-    <v-btn @click="submitTasks">Submit</v-btn>
+    <v-card class="dashboard-card smaller-card">
+      <v-card-title>Task and Date Selection</v-card-title>
+      <v-card-text>
+        <div>
+          <div v-for="task in tasks" :key="task.id">
+            <v-checkbox v-model="selectedTasks" :label="task.name" :value="task.id"></v-checkbox>
+          </div>
+        </div>
+        <div class="date-selection">
+          <v-text-field
+            v-model="startDate"
+            label="Start Date"
+            outlined
+            dense
+            type="date"
+            class="date-picker"
+          ></v-text-field>
+          <br />
+          <v-text-field
+            v-model="endDate"
+            label="End Date"
+            outlined
+            dense
+            type="date"
+            class="date-picker"
+          ></v-text-field>
+          <br />
+          <v-btn @click="submit" class="submit-button">Submit</v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+
+    <v-snackbar
+      v-model="errorSnackbar"
+      :color="snackbarColor"
+      :timeout="snackbarTimeout"
+      top
+      multi-line
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
   </v-container>
 </template>
-
-
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import { ky } from '@/lib/ky'
 
 const headers = ref([
-  { title: 'First Name', key: 'first_name' },
-  { title: 'Last Name', key: 'last_name' },
-  { title: 'Email', key: 'email' },
-])
-
-const headersTwo = ref([
-  { title: '', key: 'checkbox', sortable: false },
-  { title: 'Task Name', key: 'name' },
+  { text: 'First Name', value: 'first_name' },
+  { text: 'Last Name', value: 'last_name' },
+  { text: 'Email', value: 'email' },
 ])
 
 const tasks = ref([])
-const selectedTasks = ref([])
-
 const students = ref([])
+const selectedTasks = ref([])
+const startDate = ref('')
+const endDate = ref('')
+const errorSnackbar = ref(false)
+const snackbarTimeout = ref(4000)
+const snackbarColor = ref('error')
+const snackbarMessage = ref('')
 
 onMounted(async () => {
   const response = await ky.get('students').json()
   students.value = response.students
-  console.log(students.value)
-  await fetchTasks()
+
+  const responseTwo = await ky.get('tasks').json()
+  tasks.value = responseTwo.tasks
 })
 
-async function fetchTasks() {
-  try {
-    const response = await ky.get('tasks').json()
-    tasks.value = response.tasks
-    console.log(tasks.value);
-  } catch (error) {
-    console.error(error)
+const submit = () => {
+  const selectedTaskIds = selectedTasks.value
+  const selectedDates = {
+    startDate: startDate.value,
+    endDate: endDate.value,
   }
-}
 
-function isSelected(task) {
-  return selectedTasks.value.includes(task)
-}
-
-function toggleSelection(task) {
-  const index = selectedTasks.value.indexOf(task)
-  if (index > -1) {
-    selectedTasks.value.splice(index, 1)
+  if (startDate.value && endDate.value && startDate.value <= endDate.value) {
+    console.log('Selected task IDs:', selectedTaskIds)
+    console.log('Selected dates:', selectedDates)
   } else {
-    selectedTasks.value.push(task)
+    showErrorSnackbar('Invalid date range selected.')
   }
 }
 
-function submitTasks() {
-  console.log(selectedTasks.value)
+const showErrorSnackbar = (message) => {
+  snackbarMessage.value = message
+  errorSnackbar.value = true
 }
-
-
 </script>
 
 <style scoped>
+.dashboard-card {
+  margin-bottom: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+}
 
+.smaller-card {
+  width: 300px;
+  margin-right: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+}
+
+.date-selection {
+  margin-top: 20px;
+}
+
+.date-picker .v-input__control {
+  background-color: #f3f5f7;
+  box-shadow: none;
+  border-radius: 4px;
+}
+
+.date-picker .v-label {
+  color: #546e7a;
+}
+
+.date-picker input[type='date']::-webkit-calendar-picker-indicator {
+  filter: invert(45%) sepia(15%) saturate(458%) hue-rotate(164deg) brightness(96%) contrast(84%);
+}
+
+.submit-button {
+  background-color: #1976d2;
+  color: #ffffff;
+  border-radius: 4px;
+}
+
+.submit-button:hover {
+  background-color: #1565c0;
+}
 </style>
