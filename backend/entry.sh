3f3377e5d214be9625fba7694d04691fd8899c26
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-17 03:24:14 +0200

    Fix dockerfile entry

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#until mysql -h"mysql" -u"webte" -p"webte" -e "quit" >/dev/null 2>&1; do
-#    echo "Waiting for MySQL to be available..."
-#    sleep 1
-#done
+until mysql -h"mysql" -u"webte" -p"webte" -e "quit" >/dev/null 2>&1; do
+    echo "Waiting for MySQL to be available..."
+    sleep 1
+done
 
 if [ ! -f /var/www/html/public/migration.temp ]; then
     php artisan migrate --force
     touch /var/www/html/public/migration.temp
 fi
-
-tail -f /dev/null
